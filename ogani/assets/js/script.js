bc853f725acb7cf1a3278a76e9c5933557984cae
mahammadmehdi <147068859+mahammadmehdi@users.noreply.github.com>
2023-11-16 19:58:13 +0400

    Add files via upload

---
+++ ogani/assets/js/script.js
@@ -0,0 +1,7 @@
+const box = document.querySelector(".box")
+const bar = document.querySelector(".bar")
+
+box.addEventListener("click", function () {   
+    bar.classList.toggle("visible")
+    
+})
